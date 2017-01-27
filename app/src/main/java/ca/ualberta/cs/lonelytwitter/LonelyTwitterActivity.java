4822497ca40211cc76a7ac38b1121cc5a18f65ad
VukR <vuk@ualberta.ca>
2017-01-27 09:54:03 -0700

    clearing of tweets

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -41,7 +41,17 @@ public class LonelyTwitterActivity extends Activity {
 
 		bodyText = (EditText) findViewById(R.id.body);
 		Button saveButton = (Button) findViewById(R.id.save);
+		Button clearButton = (Button) findViewById((R.id.clear));
 		oldTweetsList = (ListView) findViewById(R.id.oldTweetsList);
+
+
+		clearButton.setOnClickListener(new View.OnClickListener(){
+			public void onClick(View v){
+				tweetList.clear();
+				adapter.notifyDataSetChanged();
+				saveInFile();
+			}
+		});
 
 		saveButton.setOnClickListener(new View.OnClickListener() {
 
